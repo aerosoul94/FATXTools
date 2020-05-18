670a4005901c4fa51f730fe2b400845c3a28f45c
aerosoul <aerosoul94@hotmail.com>
2020-05-18 05:59:05 -0500

    Disable HideSelection in tree views

--- FATXTools/Controls/RecoveryResults.Designer.cs
+++ FATXTools/Controls/RecoveryResults.Designer.cs
@@ -80,6 +80,7 @@
             // 
             this.treeView1.ContextMenuStrip = this.treeContextMenu;
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.HideSelection = false;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(412, 954);
