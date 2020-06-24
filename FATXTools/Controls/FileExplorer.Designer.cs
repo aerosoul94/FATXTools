b847f541a247f4df6582abcdcdb4f153d10ef072
aerosoul <aerosoul94@hotmail.com>
2020-06-23 19:46:22 -0500

    Reset tree view font for File Explorer

--- FATXTools/Controls/FileExplorer.Designer.cs
+++ FATXTools/Controls/FileExplorer.Designer.cs
@@ -85,7 +85,6 @@
             // 
             this.treeView1.ContextMenuStrip = this.treeContextMenu;
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.treeView1.HideSelection = false;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
