24767751c419416403cee13719422be91820b1d7
aerosoul <aerosoul94@hotmail.com>
2020-06-29 21:48:59 -0500

    Move FileAttribute enum to its own file

--- FATX/FileSystem/DirectoryEntry.cs
+++ FATX/FileSystem/DirectoryEntry.cs
@@ -4,16 +4,6 @@ using System.Text;
 
 namespace FATX.FileSystem
 {
-    [Flags]
-    public enum FileAttribute
-    {
-        ReadOnly = 0x1,
-        Hidden = 0x2,
-        System = 0x4,
-        Directory = 0x10,
-        Archive = 0x20
-    }
-
     public class DirectoryEntry
     {
         private byte _fileNameLength;
---
+++ FATX/FileSystem/FileAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FATX.FileSystem
+{
+    [Flags]
+    public enum FileAttribute
+    {
+        ReadOnly = 0x1,
+        Hidden = 0x2,
+        System = 0x4,
+        Directory = 0x10,
+        Archive = 0x20
+    }
+}
