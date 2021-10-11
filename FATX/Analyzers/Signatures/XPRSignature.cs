e6f6b785c2962ab696cdb708c87cf33e86d0d8d1
aerosoul94 <aerosoul94@users.noreply.github.com>
2021-10-11 13:33:19 -0500

    Add XPR Signature

---
+++ FATX/Analyzers/Signatures/XPRSignature.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+
+using FATX.Streams;
+
+namespace FATX.Analyzers.Signatures
+{
+    public class XPRSignature : IFileSignature
+    {
+        public string Name => "XPR";
+
+        private static readonly string XPRMagic = "XPR0";
+
+        public bool Test(CarverReader reader)
+        {
+            byte[] magic = reader.ReadBytes(4);
+            return Encoding.ASCII.GetString(magic) == XPRMagic;
+        }
+
+        public void Parse(CarverReader reader, CarvedFile carvedFile)
+        {
+            reader.Seek(0x4);
+            carvedFile.FileSize = reader.ReadUInt32();
+        }
+    }
+}
