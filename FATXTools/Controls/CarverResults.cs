43b9cbb1f222eef2fb1f8f298c68399edc88aee4
aerosoul94 <aerosoul94@users.noreply.github.com>
2021-10-11 03:58:22 -0500

    Extract CarverTask from CarverResults view

--- FATXTools/Controls/CarverResults.cs
+++ FATXTools/Controls/CarverResults.cs
@@ -1,13 +1,14 @@
-﻿using FATX.Analyzers;
-using FATX.Analyzers.Signatures;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FATX.Analyzers;
 using FATX.FileSystem;
+
 using FATXTools.Dialogs;
 using FATXTools.Utilities;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Windows.Forms;
+using FATXTools.Tasks;
 
 namespace FATXTools
 {
@@ -22,6 +23,7 @@ namespace FATXTools
 
             this._carvedFiles = files;
             this._volume = volume;
+
             PopulateResultsList(_carvedFiles);
         }
 
@@ -42,28 +44,16 @@ namespace FATXTools
             }
         }
 
-        private void SaveFile(CarvedFile signature, string path)
+        private async Task RunRecoverAllTask(string path, List<CarvedFile> files)
         {
-            const int bufsize = 0x100000;
-            var remains = signature.FileSize;
-            _volume.FileAreaStream.Seek(signature.Offset, SeekOrigin.Begin);
+            var options = new TaskDialogOptions() { Title = "Save Files" };
 
-            path = path + "/" + signature.FileName;
-            var uniquePath = Utility.UniqueFileName(path);
-            using (FileStream file = new FileStream(uniquePath, FileMode.Create))
-            {
-                while (remains > 0)
-                {
-                    var read = Math.Min(remains, bufsize);
-                    remains -= read;
-                    byte[] buf = new byte[read];
-                    _volume.FileAreaStream.Read(buf, 0, (int)read);
-                    file.Write(buf, 0, (int)read);
-                }
-            }
+            await TaskRunner.Instance.RunTaskAsync(ParentForm, options,
+                CarverTask.RunSaveAllTask(_volume, path, files)
+            );
         }
 
-        private void recoverFileToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void recoverFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (listView1.SelectedItems.Count == 0)
                 return;
@@ -72,29 +62,13 @@ namespace FATXTools
             {
                 if (fbd.ShowDialog() == DialogResult.OK)
                 {
+                    List<CarvedFile> files = new List<CarvedFile>();
                     foreach (ListViewItem item in listView1.SelectedItems)
-                    {
-                        SaveFile((CarvedFile)item.Tag, fbd.SelectedPath);
-                    }
+                        files.Add((CarvedFile)item.Tag);
+
+                    await RunRecoverAllTask(fbd.SelectedPath, files);
                 }
             }
-        }
-
-        private Action<CancellationToken, IProgress<(int, string)>> RunRecoverAllTask(string path, List<CarvedFile> files)
-        {
-            return (cancellationToken, progress) =>
-            {
-                var i = 1;
-                var count = files.Count;
-
-                foreach (var file in files)
-                {
-                    SaveFile(file, path);
-
-                    var percent = (int)(((float)i / (float)count) * 100);
-                    progress.Report((percent, $"{i}/{count}: {file.FileName}"));
-                }
-            };
         }
 
         private async void recoverAllToolStripMenuItem_Click(object sender, EventArgs e)
@@ -105,13 +79,9 @@ namespace FATXTools
                 {
                     List<CarvedFile> files = new List<CarvedFile>();
                     foreach (ListViewItem item in listView1.Items)
-                    {
                         files.Add((CarvedFile)item.Tag);
-                    }
 
-                    var options = new TaskDialogOptions() { Title = "Save File" };
-
-                    await TaskRunner.Instance.RunTaskAsync(ParentForm, options, RunRecoverAllTask(fbd.SelectedPath, files));
+                    await RunRecoverAllTask(fbd.SelectedPath, files);
                 }
             }
         }
---
+++ FATXTools/Tasks/CarverTask.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+using FATX.Analyzers;
+using FATX.FileSystem;
+
+using FATXTools.Utilities;
+
+namespace FATXTools.Tasks
+{
+    public class CarverTask
+    {
+        private Volume _volume;
+        private CancellationToken _cancellationToken;
+        private IProgress<(int, string)> _progress;
+
+        public CarverTask(Volume volume, CancellationToken cancellationToken, IProgress<(int, string)> progress)
+        {
+            _volume = volume;
+            _cancellationToken = cancellationToken;
+            _progress = progress;
+        }
+
+        public static Action<CancellationToken, IProgress<(int, string)>> RunSaveAllTask(
+            Volume volume, string path, List<CarvedFile> files)
+        {
+            return (cancellationToken, progress) =>
+            {
+                var task = new CarverTask(volume, cancellationToken, progress);
+
+                task.SaveAll(path, files);
+            };
+        }
+
+        public static Action<CancellationToken, IProgress<(int,string)>> RunSaveTask(
+            Volume volume, string path, CarvedFile file)
+        {
+            return (cancellationToken, progress) =>
+            {
+                var task = new CarverTask(volume, cancellationToken, progress);
+
+                task.Save(path, file);
+            };
+        }
+
+        public void SaveAll(string path, List<CarvedFile> files)
+        {
+            var i = 1;
+
+            foreach (var file in files)
+            {
+                WriteFile(path, file);
+
+                ReportProgress(i++, files.Count, file);
+            }
+        }
+
+        public void Save(string path, CarvedFile file)
+        {
+            WriteFile(path, file);
+        }
+
+        private void WriteFile(string path, CarvedFile file)
+        {
+            const int bufsize = 0x100000;
+            var remains = file.FileSize;
+            _volume.FileAreaStream.Seek(file.Offset, SeekOrigin.Begin);
+
+            path = path + "/" + file.FileName;
+            var uniquePath = Utility.UniqueFileName(path);
+
+            using (FileStream stream = new FileStream(uniquePath, FileMode.Create))
+            {
+                while (remains > 0)
+                {
+                    var read = Math.Min(remains, bufsize);
+                    remains -= read;
+                    byte[] buf = new byte[read];
+                    _volume.FileAreaStream.Read(buf, 0, (int)read);
+                    stream.Write(buf, 0, (int)read);
+                }
+            }
+        }
+
+        private void ReportProgress(int progress, int total, CarvedFile currentFile)
+        {
+            var percent = (int)(((float)progress / (float)total) * 100);
+            _progress.Report((percent, $"{progress}/{total}: {currentFile.FileName}"));
+        }
+    }
+}
