5105fc85872912bc9fe5481cf6f877b7e368846d
aerosoul94 <aerosoul94@users.noreply.github.com>
2021-10-10 19:26:36 -0500

    Prepare file saving tasks for refactoring.
    I need to dig into it a bit more..
    
    * Document differences between RecoveryTask and SaveContentTask.
    * Organize methods in both classes to similar orders for easy diffing.
    * Simplify TryIOOperation
    
    Definitely could use more code comments

--- FATXTools/Tasks/RecoveryTask.cs
+++ FATXTools/Tasks/RecoveryTask.cs
@@ -10,6 +10,12 @@ using FATXTools.Database;
 
 namespace FATXTools.Tasks
 {
+    /// <summary>
+    /// This class is similar to SaveContentTask and should both eventually be merged into a single class.
+    /// It is responsible for saving files all files including those that were not deleted and those that were 
+    /// recovered. Recovered files are recovered using the clusters stored in the <see cref="DatabaseFile.ClusterChain"/>
+    /// field. Stray directories are saved to folders that start with "Cluster" and have the cluster index as a suffix.
+    /// </summary>
     public class RecoveryTask
     {
         private CancellationToken cancellationToken;
@@ -27,34 +33,34 @@ namespace FATXTools.Tasks
             this.progress = progress;
         }
 
-        public void Save(string path, DatabaseFile databaseFile)
+        /// <summary>
+        /// Save a single file node to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="node">The file node to save.</param>
+        public void Save(string path, DatabaseFile node)
         {
-            numFiles = databaseFile.CountFiles();
+            numFiles = node.CountFiles();
 
-            if (databaseFile.IsDirectory())
-            {
-                SaveDirectory(databaseFile, path);
-            }
-            else
-            {
-                SaveFile(databaseFile, path);
-            }
+            Console.WriteLine($"Saving {numFiles} files.");
+
+            SaveNode(path, node);
         }
 
-        public void SaveAll(string path, List<DatabaseFile> dirents)
+        /// <summary>
+        /// Save a list of file nodes to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="nodes">The list of files to save.</param>
+        public void SaveAll(string path, List<DatabaseFile> nodes)
         {
-            numFiles = CountFiles(dirents);
+            numFiles = CountFiles(nodes);
 
-            foreach (var databaseFile in dirents)
+            Console.WriteLine($"Saving {numFiles} files.");
+
+            foreach (var node in nodes)
             {
-                if (databaseFile.IsDirectory())
-                {
-                    SaveDirectory(databaseFile, path);
-                }
-                else
-                {
-                    SaveFile(databaseFile, path);
-                }
+                SaveNode(path, node);
             }
         }
 
@@ -72,15 +78,15 @@ namespace FATXTools.Tasks
 
                 Directory.CreateDirectory(clusterDir);
 
-                foreach (var file in cluster.Value)
+                foreach (var node in cluster.Value)
                 {
-                    if (file.IsDirectory())
+                    if (node.IsDirectory())
                     {
-                        SaveDirectory(file, path);
+                        SaveDirectory(path, node);
                     }
                     else
                     {
-                        SaveFile(file, path);
+                        SaveNode(path, node);
                     }
                 }
             }
@@ -91,11 +97,11 @@ namespace FATXTools.Tasks
             // DirectoryEntry.CountFiles does not count deleted files
             long n = 0;
 
-            foreach (var databaseFile in dirents)
+            foreach (var node in dirents)
             {
-                if (databaseFile.IsDirectory())
+                if (node.IsDirectory())
                 {
-                    n += CountFiles(databaseFile.Children) + 1;
+                    n += CountFiles(node.Children) + 1;
                 }
                 else
                 {
@@ -106,76 +112,29 @@ namespace FATXTools.Tasks
             return n;
         }
 
-        private void ReportProgress()
-        {
-            var percent = (int)(((float)numSaved / (float)numFiles) * 100);
-            progress.Report((percent, $"{numSaved}/{numFiles}: {currentFile}"));
-        }
-
-        private void SaveDirectory(DatabaseFile databaseFile, string path)
-        {
-            path = path + "\\" + databaseFile.FileName;
-            //Console.WriteLine($"{path}");
-
-            currentFile = databaseFile.FileName;
-            numSaved++;
-            ReportProgress();
-
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            foreach (DatabaseFile child in databaseFile.Children)
-            {
-                if (child.IsDirectory())
-                {
-                    SaveDirectory(child, path);
-                }
-                else
-                {
-                    SaveFile(child, path);
-                }
-            }
-
-            TryIOOperation(() =>
-            {
-                DirectorySetTimestamps(path, databaseFile);
-            });
-        }
-
-        private void SaveFile(DatabaseFile databaseFile, string path)
-        {
-            path = path + "\\" + databaseFile.FileName;
-            //Console.WriteLine($"{path}");
-
-            currentFile = databaseFile.FileName;
-            numSaved++;
-            ReportProgress();
-
-            volume.ClusterReader.ReadCluster(databaseFile.FirstCluster);
-
-            TryIOOperation(() =>
-            {
-                WriteFile(path, databaseFile);
-
-                FileSetTimeStamps(path, databaseFile);
-            });
-        }
-
+        /// <summary>
+        /// Displays an error dialog window that asks whether or not to retry the IO operation.
+        /// </summary>
+        /// <param name="e">The exception that occured.</param>
+        /// <returns>The user's response as a DialogResult.</returns>
         private DialogResult ShowIOErrorDialog(Exception e)
         {
             return MessageBox.Show($"{e.Message}",
                 "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
         }
 
-        private void WriteFile(string path, DatabaseFile databaseFile)
+        /// <summary>
+        /// Writes the file's data blocks to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="node">The file node to save.</param>
+        private void WriteFile(string path, DatabaseFile node)
         {
             using (FileStream outFile = File.OpenWrite(path))
             {
-                uint bytesLeft = databaseFile.FileSize;
+                uint bytesLeft = node.FileSize;
 
-                foreach (uint cluster in databaseFile.ClusterChain)
+                foreach (uint cluster in node.ClusterChain)
                 {
                     byte[] clusterData = this.volume.ClusterReader.ReadCluster(cluster);
 
@@ -187,46 +146,139 @@ namespace FATXTools.Tasks
             }
         }
 
-        private void FileSetTimeStamps(string path, DatabaseFile databaseFile)
+        /// <summary>
+        /// Sets a file's timestamps.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <param name="node">The file node that holds the timestamps.</param>
+        private void FileSetTimeStamps(string path, DatabaseFile node)
         {
-            File.SetCreationTime(path, databaseFile.CreationTime.AsDateTime());
-            File.SetLastWriteTime(path, databaseFile.LastWriteTime.AsDateTime());
-            File.SetLastAccessTime(path, databaseFile.LastAccessTime.AsDateTime());
+            File.SetCreationTime(path, node.CreationTime.AsDateTime());
+            File.SetLastWriteTime(path, node.LastWriteTime.AsDateTime());
+            File.SetLastAccessTime(path, node.LastAccessTime.AsDateTime());
         }
 
-        private void DirectorySetTimestamps(string path, DatabaseFile databaseFile)
+        /// <summary>
+        /// Set's a directory's timestamps. This should be done after all files inside the directory
+        /// have been written.
+        /// </summary>
+        /// <param name="path">The path to the folder.</param>
+        /// <param name="node">The file node that holds the timestamps.</param>
+        private void DirectorySetTimestamps(string path, DatabaseFile node)
         {
-            Directory.SetCreationTime(path, databaseFile.CreationTime.AsDateTime());
-            Directory.SetLastWriteTime(path, databaseFile.LastWriteTime.AsDateTime());
-            Directory.SetLastAccessTime(path, databaseFile.LastAccessTime.AsDateTime());
+            Directory.SetCreationTime(path, node.CreationTime.AsDateTime());
+            Directory.SetLastWriteTime(path, node.LastWriteTime.AsDateTime());
+            Directory.SetLastAccessTime(path, node.LastAccessTime.AsDateTime());
         }
 
+        /// <summary>
+        /// Executes an IO operation until it succeeds or until the user decides not to retry.
+        /// </summary>
+        /// <param name="action">The IO operation to execute.</param>
         private void TryIOOperation(Action action)
         {
-            try
+            var dialogResult = DialogResult.None;
+
+            while (true)
             {
-                action();
-            }
-            catch (IOException e)
-            {
-                while (true)
+                try
                 {
-                    var dialogResult = ShowIOErrorDialog(e);
-
-                    if (dialogResult == DialogResult.Retry)
-                    {
-                        try
-                        {
-                            action();
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-                    }
-
-                    break;
+                    action();
                 }
+                catch (Exception e)
+                {
+                    dialogResult = ShowIOErrorDialog(e);
+                }
+
+                if (dialogResult != DialogResult.Retry)
+                    break;
+            }
+        }
+
+        private void ReportProgress()
+        {
+            var percent = (int)(((float)numSaved / (float)numFiles) * 100);
+            progress.Report((percent, $"{numSaved}/{numFiles}: {currentFile}"));
+        }
+
+        /// <summary>
+        /// Save the file node to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="node">The file node to save.</param>
+        private void SaveFile(string path, DatabaseFile node)
+        {
+            path = path + "\\" + node.FileName;
+            //Console.WriteLine(path);
+
+            currentFile = node.FileName;
+            numSaved++;
+            ReportProgress();
+
+            volume.ClusterReader.ReadCluster(node.FirstCluster);
+
+            TryIOOperation(() =>
+            {
+                WriteFile(path, node);
+
+                FileSetTimeStamps(path, node);
+            });
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+
+        /// <summary>
+        /// Save the file node for a directory to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the directory into.</param>
+        /// <param name="node">The directory's file node.</param>
+        private void SaveDirectory(string path, DatabaseFile node)
+        {
+            path = path + "\\" + node.FileName;
+            //Console.WriteLine($"{path}");
+
+            currentFile = node.FileName;
+            numSaved++;
+            ReportProgress();
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (DatabaseFile child in node.Children)
+            {
+                SaveFile(path, child);
+            }
+
+            TryIOOperation(() =>
+            {
+                DirectorySetTimestamps(path, node);
+            });
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+
+        /// <summary>
+        /// Save a file node. This will save either directory or a file depending on the node's type.
+        /// </summary>
+        /// <param name="path">The path to save the file node to.</param>
+        /// <param name="node">The file node to save.</param>
+        public void SaveNode(string path, DatabaseFile node)
+        {
+            if (node.IsDirectory())
+            {
+                SaveDirectory(path, node);
+            }
+            else
+            {
+                SaveFile(path, node);
             }
         }
     }
--- FATXTools/Tasks/SaveContentTask.cs
+++ FATXTools/Tasks/SaveContentTask.cs
@@ -7,62 +7,82 @@ using System.Windows.Forms;
 
 namespace FATXTools.Tasks
 {
+    /// <summary>
+    /// This class is similar to RecoveryTask and should both eventually be merged into a single class.
+    /// It is responsible for saving files that have not been deleted from the file system. Deleted files are
+    /// skipped (<see cref="SaveDeleted(string, DirectoryEntry)"/> 
+    /// </summary>
     public class SaveContentTask
     {
         private CancellationToken cancellationToken;
-
         private IProgress<(int, string)> progress;
-
         private Volume volume;
 
-        private string currentFile;
-
-        private int numSaved;
+        private string currentFile = String.Empty;
+        private int numSaved = 0;
         private long numFiles;
 
         public SaveContentTask(Volume volume, CancellationToken cancellationToken, IProgress<(int, string)> progress)
         {
-            currentFile = String.Empty;
-
+            this.volume = volume;
             this.cancellationToken = cancellationToken;
             this.progress = progress;
-            this.volume = volume;
-
-            this.numSaved = 0;
         }
 
-        public void Save(string path, DirectoryEntry dirent)
+        /// <summary>
+        /// Save a single file node to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="node">The file node to save.</param>
+        public void Save(string path, DirectoryEntry node)
         {
-            numFiles = dirent.CountFiles();
+            numFiles = node.CountFiles();
 
             Console.WriteLine($"Saving {numFiles} files.");
 
-            SaveDirectoryEntry(path, dirent);
+            SaveNode(path, node);
         }
 
-        public void SaveAll(string path, List<DirectoryEntry> dirents)
+        /// <summary>
+        /// Save a list of file nodes to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="nodes">The list of files to save.</param>
+        public void SaveAll(string path, List<DirectoryEntry> nodes)
         {
             numFiles = volume.CountFiles();
 
             Console.WriteLine($"Saving {numFiles} files.");
 
-            foreach (var dirent in dirents)
+            foreach (var node in nodes)
             {
-                SaveDirectoryEntry(path, dirent);
+                SaveNode(path, node);
             }
         }
 
+        /// <summary>
+        /// Displays an error dialog window that asks whether or not to retry the IO operation.
+        /// </summary>
+        /// <param name="e">The exception that occured.</param>
+        /// <returns>The user's response as a DialogResult.</returns>
         private DialogResult ShowIOErrorDialog(Exception e)
         {
             return MessageBox.Show($"{e.Message}",
                 "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
         }
 
-        private void WriteFile(string path, DirectoryEntry dirent, List<uint> chainMap)
+        /// <summary>
+        /// Writes the file's data blocks to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="node">The file node to save.</param>
+        private void WriteFile(string path, DirectoryEntry node)
         {
             using (FileStream outFile = File.OpenWrite(path))
             {
-                uint bytesLeft = dirent.FileSize;
+                uint bytesLeft = node.FileSize;
+
+                var chainMap = this.volume.FileAllocationTable.GetClusterChain(node);
 
                 foreach (uint cluster in chainMap)
                 {
@@ -75,46 +95,53 @@ namespace FATXTools.Tasks
                 }
             }
         }
-        private void FileSetTimeStamps(string path, DirectoryEntry dirent)
+
+        /// <summary>
+        /// Sets a file's timestamps.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        /// <param name="node">The file node that holds the timestamps.</param>
+        private void FileSetTimeStamps(string path, DirectoryEntry node)
         {
-            File.SetCreationTime(path, dirent.CreationTime.AsDateTime());
-            File.SetLastWriteTime(path, dirent.LastWriteTime.AsDateTime());
-            File.SetLastAccessTime(path, dirent.LastAccessTime.AsDateTime());
+            File.SetCreationTime(path, node.CreationTime.AsDateTime());
+            File.SetLastWriteTime(path, node.LastWriteTime.AsDateTime());
+            File.SetLastAccessTime(path, node.LastAccessTime.AsDateTime());
         }
 
-        private void DirectorySetTimestamps(string path, DirectoryEntry dirent)
+        /// <summary>
+        /// Set's a directory's timestamps. This should be done after all files inside the directory
+        /// have been written.
+        /// </summary>
+        /// <param name="path">The path to the folder.</param>
+        /// <param name="node">The file node that holds the timestamps.</param>
+        private void DirectorySetTimestamps(string path, DirectoryEntry node)
         {
-            Directory.SetCreationTime(path, dirent.CreationTime.AsDateTime());
-            Directory.SetLastWriteTime(path, dirent.LastWriteTime.AsDateTime());
-            Directory.SetLastAccessTime(path, dirent.LastAccessTime.AsDateTime());
+            Directory.SetCreationTime(path, node.CreationTime.AsDateTime());
+            Directory.SetLastWriteTime(path, node.LastWriteTime.AsDateTime());
+            Directory.SetLastAccessTime(path, node.LastAccessTime.AsDateTime());
         }
 
+        /// <summary>
+        /// Executes an IO operation until it succeeds or until the user decides not to retry.
+        /// </summary>
+        /// <param name="action">The IO operation to execute.</param>
         private void TryIOOperation(Action action)
         {
-            try
+            var dialogResult = DialogResult.None;
+
+            while (true)
             {
-                action();
-            }
-            catch (IOException e)
-            {
-                while (true)
+                try
                 {
-                    var dialogResult = ShowIOErrorDialog(e);
-
-                    if (dialogResult == DialogResult.Retry)
-                    {
-                        try
-                        {
-                            action();
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-                    }
-
-                    break;
+                    action();
                 }
+                catch (Exception e)
+                {
+                    dialogResult = ShowIOErrorDialog(e);
+                }
+
+                if (dialogResult != DialogResult.Retry)
+                    break;
             }
         }
 
@@ -124,23 +151,26 @@ namespace FATXTools.Tasks
             progress.Report((percent, $"{numSaved}/{numFiles}: {currentFile}"));
         }
 
-        private void SaveFile(string path, DirectoryEntry dirent)
+        /// <summary>
+        /// Save the file node to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the file to.</param>
+        /// <param name="node">The file node to save.</param>
+        private void SaveFile(string path, DirectoryEntry node)
         {
-            path = path + "\\" + dirent.FileName;
+            path = path + "\\" + node.FileName;
             Console.WriteLine(path);
 
             // Report where we are at
-            currentFile = dirent.FileName;
+            currentFile = node.FileName;
             numSaved++;
             ReportProgress();
 
-            List<uint> chainMap = this.volume.FileAllocationTable.GetClusterChain(dirent);
-
             TryIOOperation(() =>
             {
-                WriteFile(path, dirent, chainMap);
+                WriteFile(path, node);
 
-                FileSetTimeStamps(path, dirent);
+                FileSetTimeStamps(path, node);
             });
 
             if (cancellationToken.IsCancellationRequested)
@@ -149,26 +179,34 @@ namespace FATXTools.Tasks
             }
         }
 
-        private void SaveDirectory(string path, DirectoryEntry dirent)
+        /// <summary>
+        /// Save the file node for a directory to the specified path.
+        /// </summary>
+        /// <param name="path">The path to save the directory into.</param>
+        /// <param name="node">The directory's file node.</param>
+        private void SaveDirectory(string path, DirectoryEntry node)
         {
-            path = path + "\\" + dirent.FileName;
+            path = path + "\\" + node.FileName;
             Console.WriteLine(path);
 
-            // Report where we are at
-            currentFile = dirent.FileName;
+            // Report our current progress
+            currentFile = node.FileName;
             numSaved++;
             ReportProgress();
 
-            Directory.CreateDirectory(path);
-
-            foreach (DirectoryEntry child in dirent.Children)
+            if (!Directory.Exists(path))
             {
-                SaveDirectoryEntry(path, child);
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (DirectoryEntry child in node.Children)
+            {
+                SaveFile(path, child);
             }
 
             TryIOOperation(() =>
             {
-                DirectorySetTimestamps(path, dirent);
+                DirectorySetTimestamps(path, node);
             });
 
             if (cancellationToken.IsCancellationRequested)
@@ -177,30 +215,41 @@ namespace FATXTools.Tasks
             }
         }
 
-        private void SaveDeleted(string path, DirectoryEntry dirent)
+        /// <summary>
+        /// Processes a deleted file. This shouldn't do anything since we don't expect to have information
+        /// for this deleted file.
+        /// </summary>
+        /// <param name="path">The path to save the deleted file.</param>
+        /// <param name="node">The file node for this deleted file.</param>
+        private void SaveDeleted(string path, DirectoryEntry node)
         {
-            path = path + "\\" + dirent.FileName;
+            path = path + "\\" + node.FileName;
 
-            currentFile = dirent.GetFullPath();
+            currentFile = node.GetFullPath();
 
             Console.WriteLine($"{path}: Cannot save deleted files.");
         }
 
-        private void SaveDirectoryEntry(string path, DirectoryEntry dirent)
+        /// <summary>
+        /// Save a file node. This will save either directory or a file depending on the node's type.
+        /// </summary>
+        /// <param name="path">The path to save the file node to.</param>
+        /// <param name="node">The file node to save.</param>
+        private void SaveNode(string path, DirectoryEntry node)
         {
-            if (dirent.IsDeleted())
+            if (node.IsDeleted())
             {
-                SaveDeleted(path, dirent);
+                SaveDeleted(path, node);
                 return;
             }
 
-            if (dirent.IsDirectory())
+            if (node.IsDirectory())
             {
-                SaveDirectory(path, dirent);
+                SaveDirectory(path, node);
             }
             else
             {
-                SaveFile(path, dirent);
+                SaveFile(path, node);
             }
         }
     }
