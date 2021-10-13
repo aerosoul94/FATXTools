1c5484a798ac60313fbce5d2384d96c11d43420d
aerosoul94 <aerosoul94@users.noreply.github.com>
2021-10-12 22:00:16 -0500

    Add comments to FatType and Platform

--- FATX/FileSystem/FatType.cs
+++ FATX/FileSystem/FatType.cs
@@ -1,8 +1,18 @@
 namespace FATX.FileSystem
 {
+    /// <summary>
+    /// The File Allocation Table type.
+    /// </summary>
     public enum FatType
     {
+        /// <summary>
+        /// 16-bit File Allocation Table
+        /// </summary>
         Fat16,
+
+        /// <summary>
+        /// 32-bit File Allocation Table
+        /// </summary>
         Fat32
     }
 }
--- FATX/FileSystem/Platform.cs
+++ FATX/FileSystem/Platform.cs
@@ -2,7 +2,14 @@ namespace FATX.FileSystem
 {
     public enum Platform
     {
+        /// <summary>
+        /// Little Endian / Original Xbox
+        /// </summary>
         Xbox,
+
+        /// <summary>
+        /// Big Endian / Xbox 360
+        /// </summary>
         X360
     }
 }
